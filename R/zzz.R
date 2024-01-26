7e6d1d2cf422c341b01d61d167ec5f72853872ca
lshep <lori.shepherd@roswellpark.org>
2024-01-26 07:26:55 -0500

    Deprecation
      - Functionality has been moved to VariantAnnotation

--- R/zzz.R
+++ R/zzz.R
@@ -6,4 +6,13 @@
     tryCatch(check <- system2(.getVepPath(),
         stdout=TRUE, stderr=TRUE),
         error=function(e) packageStartupMessage(msg))
+
+
+    msg <- sprintf(
+        "Package '%s' is deprecated and will be removed from Bioconductor
+         version %s", pkgname,
+        "3.20; Functionality has been moved to VariantAnnotation")
+    .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
+
+
 }
